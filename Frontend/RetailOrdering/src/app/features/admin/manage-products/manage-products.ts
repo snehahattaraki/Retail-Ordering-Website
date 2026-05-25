67beeb0bc32397be5edaa285b86203d358424223
SahanaPadaganur <padaganursuresh@gmail.com>
2026-05-25 12:18:36 +0530

    Files are created for frontend

---
+++ Frontend/RetailOrdering/src/app/features/admin/manage-products/manage-products.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-products',
+  imports: [],
+  templateUrl: './manage-products.html',
+  styleUrl: './manage-products.css',
+})
+export class ManageProducts {}
---
+++ Frontend/RetailOrdering/src/app/features/payments/payment-page/payment-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-page',
+  imports: [],
+  templateUrl: './payment-page.html',
+  styleUrl: './payment-page.css',
+})
+export class PaymentPage {}
